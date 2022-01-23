5b7ca175374709b4e836da4ea2499279cc419945
zachasyl <sylvane.z@northeastern.edu>
2022-01-23 17:01:36 -0500

    added ! to hello world text and added button with toast

--- app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/MainActivity.java
+++ app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/MainActivity.java
@@ -2,13 +2,30 @@ package neu.edu.madcourse.numadfa_zacharysylvane;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Zachary Sylvane" + "\n" + "sylvane.z@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
