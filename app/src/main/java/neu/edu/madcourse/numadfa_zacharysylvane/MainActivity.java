339d3bce5d1ebbc14ec998865d5d3c83b67abd1e
zachasyl <sylvane.z@northeastern.edu>
2022-02-01 16:03:30 -0500

    updated mainactivity with event listener, can navigate to detailactivity

--- app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/MainActivity.java
+++ app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/MainActivity.java
@@ -3,6 +3,7 @@ package neu.edu.madcourse.numadfa_zacharysylvane;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,18 +12,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    Button newActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button1);
+        newActivity = (Button) findViewById(R.id.button2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),
                         "Zachary Sylvane" + "\n" + "sylvane.z@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        newActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
             }
         });
 
