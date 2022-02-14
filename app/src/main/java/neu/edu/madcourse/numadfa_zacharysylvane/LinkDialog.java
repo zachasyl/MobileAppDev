b086d3b943ee54518b68c1a1bc5e725f350596ab
zachasyl <sylvane.z@northeastern.edu>
2022-02-14 12:45:39 -0500

    dialog for entering http

---
+++ app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/LinkDialog.java
@@ -0,0 +1,62 @@
+package neu.edu.madcourse.numadfa_zacharysylvane;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class LinkDialog extends AppCompatDialogFragment {
+    private EditText editTextURL;
+    private EditText uName;
+    private LinkDialogListener listener;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view= inflater.inflate(R.layout.layout_dialog, null);
+
+        builder.setView(view);
+        builder.setTitle("Enter new bookmark")      ;
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+
+        });
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                editTextURL = view.findViewById(R.id.url);
+                uName = view.findViewById((R.id.url2));
+                String url = editTextURL.getText().toString();
+                String urlName = uName.getText().toString();
+
+                listener.applyTexts(url, urlName);
+
+            }
+        });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (LinkDialogListener) context;
+        } catch (Exception e) {
+            throw new ClassCastException(context.toString() + "m");
+        }
+    }
+
+
+
+}
