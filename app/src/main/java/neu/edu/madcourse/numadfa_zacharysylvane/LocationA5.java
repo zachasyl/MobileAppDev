dff628649cc6c1aa7323705c5090fcf8e4ad7fd6
zachasyl <sylvane.z@northeastern.edu>
2022-02-19 19:15:36 -0500

    complete/works on emulator. simplified by removing my dialogs. since fetchlocation is on click, need to hit button twice first time accepting this needs to be fixed

--- app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/LocationA5.java
+++ app/src/main/java/neu/edu/madcourse/numadfa_zacharysylvane/LocationA5.java
@@ -1,9 +1,11 @@
 package neu.edu.madcourse.numadfa_zacharysylvane;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -20,6 +22,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+
 public class LocationA5 extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
     Button fetch_location;
@@ -34,8 +37,8 @@ public class LocationA5 extends AppCompatActivity {
         fetch_location = (Button) findViewById(R.id.fetch_location);
         user_location = findViewById(R.id.user_location);
         myFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-
+        Double latitude;
+        Double longitude;
 
         fetch_location.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,62 +46,42 @@ public class LocationA5 extends AppCompatActivity {
 
                 fetchLocation();
 
-
-
             }
         });
 
     }
-
     private void fetchLocation() {
+        // Permission already granted
         if (ContextCompat.checkSelfPermission(LocationA5.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission  not granted
-
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(LocationA5.this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-
-
-                // user has denied the first time. now we are at time 2,3,4, etc
-
-
-            } else {
-                // request the permission (this is the first time)
-                ActivityCompat.requestPermissions(LocationA5.this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-
-              
-            }
-        } else {//permission granted therefore we wont get following error
+                == PackageManager.PERMISSION_GRANTED) {
+            //permission granted therefore we wont get following error
 
 //            Call requires permission which may be rejected by user: code should explicitly
 //            check to see if permission is available (with checkPermission) or explicitly
 //            handle a potential SecurityException
 
-            user_location.setText(" A");
             myFusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
                         public void onSuccess(Location location) {
                             // Got last known location.
                             if (location != null) {
-                                // Logic to handle location object
-                                Double latittude = location.getLatitude();
+                                Double latitude = location.getLatitude();
                                 Double longitude = location.getLongitude();
 
-                                user_location.setText("Latitude = "+latittude + "\nLongitude = " + longitude);
+                                user_location.setText("Latitude = " + latitude + "\nLongitude = " + longitude);
 
                             }
                         }
                     });
 
+        }else{ //not granted
 
-
-
+            ActivityCompat.requestPermissions(LocationA5.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
         }
     }
-
 }
+
